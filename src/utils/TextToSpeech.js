f09d23a62a7d77bab5e6c6b82fb9174018691496
Bhagavath Sai Ananthu <122680723+Bhagavathsai23@users.noreply.github.com>
2025-02-16 17:21:21 +0530

    Update TextToSpeech.js

--- src/utils/TextToSpeech.js
+++ src/utils/TextToSpeech.js
@@ -1,1 +1,8 @@
+const TextToSpeech = (text) => {
+  const speech = new SpeechSynthesisUtterance(text);
+  speech.lang = "en-US";
+  window.speechSynthesis.speak(speech);
+};
+
+export default TextToSpeech;
 
